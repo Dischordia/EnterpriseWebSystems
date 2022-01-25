472401c90ec98f2b994693ae5aeeaa8406ad33e1
Dischordia <55534884+Dischordia@users.noreply.github.com>
2022-01-25 15:57:07 +0000

    Added admin pathing to the server

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ var express = require('express')
 var app = express();
 const PORT = process.env.PORT || 8080;
 
-// set the port based on environemnt *more on environments later)
+// set the port based on environment *more on environments later)
 var port = PORT;
 
 // send our index.html file to the user of our the home page
@@ -11,6 +11,34 @@ app.get('/', function(req,res){
 	res.sendFile(__dirname + '/index.html');
 });
 
+
+
+// ########## ADMIN SECTION ##########
+// create routes for the admin section
+// get an instance of the router
+var adminRouter = express.Router();
+
+// admin main page. the dashboard (http://localhost:PORT.admin)
+adminRouter.get('/', function(req, res){
+	res.send('I am the dashboard!'); 
+});
+
+// users page (http://localhost:PORT/admin/users)
+adminRouter.get('/users', function(req, res)){
+	res.send('I show all the users!'); 
+});
+
+// posts page (http://localhost:PORT/admin/posts)
+adminRouter.get('/posts', function(req, res)){
+	res.send('I show all the posts!');
+});
+
+// apply the routes to the applicaiton
+app.use('/admin', adminRouter);
+
+
+
+// ########## SERVER INITIALISATION ##########
 // start the server
 app.listen(PORT);
 console.log('Express Server running at http://127.0.0.1:' + PORT);
